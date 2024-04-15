cb6f45178729846e697218db9186553ed1b045e1
danutavram <avramnicolaedan@gmail.com>
2024-04-15 22:57:14 +0300

    Twig

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -18,7 +18,7 @@ class HelloController extends AbstractController
         ]);
     }
 
-    #[Route('/messages/{id<\d+>}', name: "app_show_one")]
+    #[Route('/messages/{id<\d+>}', name: "app_show")]
     public function showOne(int $id): Response
     {
         return $this->render(
